e17ebe9d238073dceef88dc2bcfdc5ad1b45740b
y_yamazaki <y_yamazaki@otafuku-lab.co>
2019-08-09 16:49:37 +0900

    add ImbalancedSampler

---
+++ pipeline/samplers.py
@@ -0,0 +1,44 @@
+import torch
+import torchvision
+
+
+class ImbalancedSampler(torch.utils.data.sampler.Sampler):
+    """Samples elements randomly from a given list of indices for imbalanced dataset
+    https://github.com/ufoym/imbalanced-dataset-sampler/blob/master/sampler.py
+    Arguments:
+        indices (list, optional): a list of indices
+        num_samples (int, optional): number of samples to draw
+    """
+
+    def __init__(self, dataset):
+        self.indices = list(range(len(dataset)))
+        self.num_samples = len(self.indices)
+
+        label_to_count = {}
+        for idx in self.indices:
+            label = self._get_label(dataset, idx)
+            if label in label_to_count:
+                label_to_count[label] += 1
+            else:
+                label_to_count[label] = 1
+
+        # weight for each sample
+        weights = [1.0 / label_to_count[self._get_label(dataset, idx)]
+                   for idx in self.indices]
+        self.weights = torch.DoubleTensor(weights)
+
+    def _get_label(self, dataset, idx):
+        dataset_type = type(dataset)
+        if dataset_type is torchvision.datasets.MNIST:
+            return dataset.train_labels[idx].item()
+        elif dataset_type is torchvision.datasets.ImageFolder:
+            return dataset.imgs[idx][1]
+        else:
+            raise NotImplementedError
+                
+    def __iter__(self):
+        return (self.indices[i] for i in torch.multinomial(
+            self.weights, self.num_samples, replacement=True))
+
+    def __len__(self):
+        return self.num_samples
