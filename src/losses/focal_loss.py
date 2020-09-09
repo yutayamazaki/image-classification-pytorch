d74a0ab6831ce737e406f4181015db24cb2d3df7
yutayamazaki <tppymd@gmail.com>
2020-09-10 01:46:30 +0900

    Fix FocalLoss

--- src/losses/focal_loss.py
+++ src/losses/focal_loss.py
@@ -1,39 +1,35 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 
 class FocalLoss(nn.Module):
     """ Focal Loss: https://arxiv.org/abs/1708.02002
-
     Args:
         gamma (float): It modulates the loss for each classes.
-
-        size_average (bool): If True, it returns loss.mean(),
-                             else it returns loss.sum().
+        size_average (bool): If True, it returns mean, else it returns sum.
     """
-
-    def __init__(self, gamma: float = 2.0, size_average: bool = True):
+    def __init__(self, gamma: float = 2.0, size_average: bool = True) -> None:
         super(FocalLoss, self).__init__()
         self.gamma: float = gamma
         self.size_average: bool = size_average
+        self.cross_entropy = nn.CrossEntropyLoss(reduction='none')
 
-    def forward(
-        self, y_true: torch.Tensor, y_pred: torch.Tensor
+    def forward(  # type: ignore
+        self,
+        outputs: torch.Tensor,
+        targets: torch.Tensor
     ) -> torch.Tensor:
         """
         Args:
-            y_true (torch.Tensor): ground truths with shape (B, ).
-
-            y_pred (torch.Tensor): Predicted label with shape (B, num_classes).
-
+            targets (torch.Tensor) : Truth label with shape (B, ).
+            outputs (torch.Tensor): Prediction result with shape
+                (B, num_classes).
         Returns:
-            torch.Tensor: Computed FocalLoss.
+            torch.Tensor: Calculated FocalLoss.
         """
-        pt = F.log_softmax(y_pred, dim=1).exp()
-        loss = -1 * ((1 - pt)**self.gamma * torch.log(pt))
-
+        logpt = self.cross_entropy(outputs, targets)
+        pt = torch.exp(-logpt)
+        loss = ((1 - pt) ** self.gamma) * logpt  # type: ignore
         if self.size_average:
             return loss.mean()
-        else:
-            return loss.sum()
+        return loss.sum()
--- src/tests/test_losses.py
+++ src/tests/test_losses.py
@@ -10,12 +10,11 @@ class FocalLossTest(unittest.TestCase):
     def setUp(self) -> None:
         self.criterion = losses.FocalLoss()
 
-    def tearDown(self) -> None:
-        pass
-
     def test_loss(self):
-        y_true = torch.Tensor([0, 0, 1])
-        y_pred = torch.Tensor([[1.0, 0.0], [1.0, 0.0], [0.5, 0.5]])
+        targets: torch.Tensor = torch.Tensor([0, 0, 1]).long()
+        outputs: torch.Tensor = torch.Tensor([
+            [1.0, 0.0], [1.0, 0.0], [0.5, 0.5]
+        ])
 
-        loss = self.criterion(y_true, y_pred)
-        self.assertAlmostEqual(loss.item(), 0.299271047)
+        loss: torch.Tensor = self.criterion(outputs, targets)
+        self.assertEqual(loss.size(), torch.Size(()))
